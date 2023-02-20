3a81eea37ac30f2b3782c4b7d6a3391770b3887e
Luis Miguel <luismiguel.molina@campusfp.es>
2023-02-20 13:35:14 +0100

    servicios terminado

--- src/app/servicios/conexion.service.ts
+++ src/app/servicios/conexion.service.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs/internal/Observable';
+import { Pedidos } from '../modelos/pedidos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConexionService {
   private url = "http://localhost:3000";
+  private id: number;
+  constructor(private http: HttpClient) {
+    this.id = 0;
+  }
 
-  constructor(private http: HttpClient) { }
-
-  getPedidosCliente(id: number, correo: string): Observable<Number> {
-    if (id) {
-      return this.http.get<number>(this.url + "/pedidos?id=" + id);
-    } else {
-      return this.http.get<number>(this.url + "/pedidos?correo=" + correo);
-    }
+  getPedidosCliente(id: number, correo: string): Promise<number> {
+    let promesa = new Promise<number>((resolve, reject) => {
+      if (id) {
+        this.id = id;
+        this.http.get<any>(this.url + "/pedidos?id=" + id).subscribe((data: any) => {
+          resolve(data.total);
+        });
+      } else {
+        this.http.get<any>(this.url + "/pedidos?correo=" + correo).subscribe((data: any) => {
+          this.id = data.id;
+          resolve(data.total);
+        });
+      }
+    });
+    return promesa;
   }
 
   getPedidosTotal(): Observable<Number> {
     return this.http.get<number>(this.url + "/pedidos");
+  }
+
+  getUltimosPedidos(): Observable<Pedidos[]> {
+    return this.http.post<Pedidos[]>(this.url + "/pedidos", this.id);
   }
 
 }
