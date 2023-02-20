804a115c5e4c486091646a45ff8e27b0740bc8fa
Luis Miguel <luismiguel.molina@campusfp.es>
2023-02-20 13:35:36 +0100

    servicio terminado

---
+++ src/app/modelos/pedidos.ts
@@ -0,0 +1,9 @@
+export class Pedidos {
+    public precio: number;
+    public fecha: Date;
+
+    constructor(precio: number, fecha: Date) {
+        this.precio = precio;
+        this.fecha = fecha;
+    }
+}
